722bb9bc8f928a27754995caa9459bf0706ada56
Maicol13montoya <montoyasalamancamaicolfabian@gmail.com>
2025-06-05 16:44:08 -0500

    INDEX ACTUALIZADO

--- index.php
+++ index.php
@@ -1,26 +1,20 @@
 <?php
-      
-    //metodo para iniciar una sesion en el navegador  
-    session_start();  
+session_start();
 
-	require 'providers/Database.php';
-     //el primer controlador por abrir siempre es el logincontroller
-	$controller = 'IndexController';
+require 'providers/Database.php';
 
-	if(!isset($_REQUEST['controller'])) {
-		require "controllers/" . $controller . ".php";
+$controller = 'IndexController';
 
-		$controller = ucwords($controller);
-		$controller = new $controller;
-		$controller->index();		
-	} else {
-		$controller = ucwords($_REQUEST['controller'].'Controller');
-		//Condicional Ternario   condición       Si es Verdad         Si es Falso
-		$method = isset($_REQUEST['method']) ? $_REQUEST['method'] : 'index';
+if (!isset($_REQUEST['controller'])) {
+	require "controllers/" . $controller . ".php";
+	$controller = new $controller;
+	$controller->index();
+} else {
+	$controller = ucfirst($_REQUEST['controller']) . 'Controller';
+	$method = isset($_REQUEST['method']) ? $_REQUEST['method'] : 'index';
 
-		require "controllers/". $controller	.".php";
-		$controller = new $controller;
+	require "controllers/" . $controller . ".php";
+	$controller = new $controller;
 
-		//Función para llamar al controlados y ejecutar el metodo.
-		call_user_func(array($controller, $method));
-	}
+	call_user_func(array($controller, $method));
+}

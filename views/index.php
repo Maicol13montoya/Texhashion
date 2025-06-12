fe990335244031bf69b14e9104aa6c81769900ff
Maicol13montoya <montoyasalamancamaicolfabian@gmail.com>
2025-06-12 16:29:10 -0500

    Update index.php
    html encabezado

--- views/index.php
+++ views/index.php
@@ -1,20 +1,16 @@
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <title>Inicio - TexFashion</title>
     <link rel="icon" href="../assets/img/TexFashion.png" sizes="32x32" type="image/png">
-
-
     <link rel="stylesheet" href="assets/css/inicio.css">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
 </head>
+
 
 <body>
 

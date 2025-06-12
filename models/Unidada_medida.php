acb47651c6cf8ecfafa30f0d59810ab07ec2d2d7
Maicol13montoya <montoyasalamancamaicolfabian@gmail.com>
2025-06-12 15:45:10 -0500

    Update Unidada_medida.php
    model uni medda exente

--- models/Unidada_medida.php
+++ models/Unidada_medida.php
@@ -1,18 +1,18 @@
 <?php
-
 /**
- * 
+ * Clase para gestionar unidad de medida
  */
 class UnidadaMedida
 {
 	private $MedidaID;
 	private $Estados;
 	private $pdo;
-	
-	function __construct()
+
+	// Constructor con visibilidad explícita
+	public function __construct()
 	{
 		try {
-			$this->pdo =new Database;
+			$this->pdo = new Database;
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
@@ -22,54 +22,52 @@ class UnidadaMedida
 	{
 		try {
 			$strSql = "SELECT * FROM unidadmedida";
-			$query =$this->pdo->select($strSql);
+			$query = $this->pdo->select($strSql);
 			return $query;
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
-	}	
+	}
 
 	public function newUnidadMedida($data)
 	{
-
 	    try {
-	    	$this->pdo->insert('unidadmedida',$data);
+	    	$this->pdo->insert('unidadmedida', $data);
 			return true;
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
-
 	}
 
 	public function getUnidadMedidaById($MedidaID)
-		{
-			try {
-				$strSql = "SELECT * FROM unidadmedida WHERE MedidaID = :MedidaID";
-				$arrayData = ['MedidaID' => $MedidaID];
-				$query = $this->pdo->select($strSql, $arrayData);
-				return $query; 
-			} catch(PDOException $e) {
-				die($e->getMessage());
-			}	
+	{
+		try {
+			$strSql = "SELECT * FROM unidadmedida WHERE MedidaID = :MedidaID";
+			$arrayData = ['MedidaID' => $MedidaID];
+			$query = $this->pdo->select($strSql, $arrayData);
+			return $query;
+		} catch (PDOException $e) {
+			die($e->getMessage());
 		}
+	}
 
-		public function editUnidadMedida($data)
-		{
-			try {
-				$strWhere = 'MedidaID = '. $data['MedidaID'];
-				$this->pdo->update('unidadmedida', $data, $strWhere);				
-			} catch(PDOException $e) {
-				die($e->getMessage());
-			}		
+	public function editUnidadMedida($data)
+	{
+		try {
+			$strWhere = 'MedidaID = ' . $data['MedidaID'];
+			$this->pdo->update('unidadmedida', $data, $strWhere);
+		} catch (PDOException $e) {
+			die($e->getMessage());
 		}
+	}
 
-		public function deleteUnidadMedida($data)
-		{
-			try {
-				$strWhere = 'MedidaID = '. $data['MedidaID'];
-				$this->pdo->delete('unidadmedida', $strWhere);
-			} catch(PDOException $e) {
-				die($e->getMessage());
-			}	
+	public function deleteUnidadMedida($data)
+	{
+		try {
+			$strWhere = 'MedidaID = ' . $data['MedidaID'];
+			$this->pdo->delete('unidadmedida', $strWhere);
+		} catch (PDOException $e) {
+			die($e->getMessage());
 		}
+	}
 }

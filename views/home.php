7b830d9d50674e683e56c05e3fcaaa6fd0282868
Maicol13montoya <montoyasalamancamaicolfabian@gmail.com>
2025-06-13 15:28:44 -0500

    Update home.php
    home

--- views/home.php
+++ views/home.php
@@ -3,44 +3,51 @@
 <head>
     <title>Inicio - TexFashion</title>
     <?php include_once 'layouts/head.php'; ?>
+    
     <script>
-        var height = window.innerHeight - 2;
-        var porh = (height * 74 / 100);
-        $(document).ready(function () {
+        document.addEventListener('DOMContentLoaded', function () {
+            var height = window.innerHeight - 2;
+            var porh = (height * 74 / 100);
             $('#info').css('height', porh);
         });
     </script>
 </head>
 <body>
     <div class="wrapper">
-        <?php include_once('layouts/sidebar.php'); ?>
+        <?php include_once 'layouts/sidebar.php'; ?>
+
         <div class="main">
-            <?php include_once('layouts/nadvar.php'); ?>
+            <?php include_once 'layouts/nadvar.php'; ?>
+
             <main class="content">
                 <div class="container-fluid p-0">
                     <?php
                     if (isset($content) && !empty($content)) {
                         echo $content;
                     } else {
-                        include_once('inicio.php');
+                        include_once 'inicio.php';
                     }
                     ?>
                 </div>
             </main>
-            <?php include_once('layouts/footer.php'); ?>
+
+            <?php include_once 'layouts/footer.php'; ?>
         </div>
     </div>
+
     <script>
-        let btnDisabled = document.getElementsByClassName('btnDisabled');
-        for (let i = 0; i < btnDisabled.length; i++) {
-            btnDisabled[i].addEventListener('click', function () {
-                Swal.fire(
-                    'No disponible',
-                    'Intenta más tarde',
-                    'info'
-                );
-            });
-        }
+        document.addEventListener('DOMContentLoaded', function () {
+            const btnDisabled = document.getElementsByClassName('btnDisabled');
+            for (let i = 0; i < btnDisabled.length; i++) {
+                btnDisabled[i].addEventListener('click', function () {
+                    Swal.fire(
+                        'No disponible',
+                        'Intenta más tarde',
+                        'info'
+                    );
+                });
+            }
+        });
     </script>
 </body>
 </html>

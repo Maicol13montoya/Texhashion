98fb18783a0ecce2e3b7f1b7cb1ce611bddceae5
Maicol13montoya <montoyasalamancamaicolfabian@gmail.com>
2025-06-13 15:18:26 -0500

    Update home.php
    solution

--- views/home.php
+++ views/home.php
@@ -2,26 +2,22 @@
 <html lang="en">
 <head>
     <title>Inicio - TexFashion</title>
-    </head>
     <?php include_once('layouts/head.php'); ?>
-
-
-<script>
-    var height = window.innerHeight - 2;
-    var porh = (height * 74 / 100);
-    $(document).ready(function () {
-        $('#info').css('height', porh);
-    });
-</script>
+    <script>
+        var height = window.innerHeight - 2;
+        var porh = (height * 74 / 100);
+        $(document).ready(function () {
+            $('#info').css('height', porh);
+        });
+    </script>
+</head>
 <body>
     <div class="wrapper">
-        <?php
-        include_once('layouts/sidebar.php');
-        ?>
+        <?php include_once('layouts/sidebar.php'); ?>
+
         <div class="main">
-            <?php
-            include_once('layouts/nadvar.php');
-            ?>
+            <?php include_once('layouts/nadvar.php'); ?>
+
             <main class="content">
                 <div class="container-fluid p-0">
                     <?php
@@ -33,11 +29,11 @@
                     ?>
                 </div>
             </main>
-            <?php
-            include_once('layouts/footer.php');
-            ?>
+
+            <?php include_once('layouts/footer.php'); ?>
         </div>
     </div>
+
     <script>
         let btnDisabled = document.getElementsByClassName('btnDisabled');
         for (let i = 0; i < btnDisabled.length; i++) {
